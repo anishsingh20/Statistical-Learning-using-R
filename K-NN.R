181dd95db41685db484a1f3e05775b9b31b6da19
Ansih Singh Walia <loveeanish@gmail.com>
2017-04-21 02:42:26 +0530

    K-NN algorithm implemented on Smarket DataSet

---
+++ K-NN.R
@@ -0,0 +1,61 @@
+#IMPLEMENTING K-NN(LAZY LEARNER) IN R
+#K-NN is a lazy learner and a Simple algorithm , but works good most of the times due to 
+#the simple  inductive bias it has.
+
+#We find out the closest neighbouring points(Xi) to the Query(test) point 'q' using a 
+#Distance metric and classify to that point -Nearest Neighbour in training data,for k=1
+
+
+#package for k-NN
+require(ISLR)
+library(class)
+library(dplyr)
+?knn
+#Classifiaction of Smarket Data set
+attach(Smarket)
+
+#Training Data Set-consisting of Predictors Lag1 and Lag2
+Smarket %>% filter(Year < 2005) %>%  
+  select(Lag1,Lag2) ->train_set
+
+#Test Data Set
+Smarket %>% filter(Year == 2005) %>%  
+  select(Lag1,Lag2) ->test_set
+
+
+model1<-knn(train_set,test_set ,Direction[Year<2005],k=1)
+#model1 returns some class labels for the Test points=test_set using Euclidean distance
+head(model1)
+
+#accuracy of the model-CONFUSION MATRIX
+table(Predicted=model1,True=Direction[Year==2005])
+#True positives and negetives and perfomance of the model
+mean(model1==Direction[Year==2005])
+#for 1-NN , accuracy is 50% and error=50% , Poor perfomance
+
+
+
+
+#Model2-3-NN
+model2<-knn(train_set,test_set ,Direction[Year<2005],k=3)
+            
+table(Predicted=model2,True=Direction[Year==2005])
+#True positives and negetives and perfomance of the model
+mean(model2==Direction[Year==2005])           
+#accuracy improved to 53%, i.e does slightly better than chance. as Error<50%
+
+
+
+
+#Model3-
+
+
+model3<-knn(train_set,test_set ,Direction[Year<2005],k=100)
+
+table(Predicted=model3,True=Direction[Year==2005])
+#True positives and negetives and perfomance of the model
+mean(model3==Direction[Year==2005])   
+
+#NOTE-100 NN gives accuracy of 55%
+#200-NN set gives accuracy of 57%
+#300-NN gives accuracy of 61%(highest)
