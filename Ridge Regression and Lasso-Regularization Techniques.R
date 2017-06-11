98d3365152f7e9e0c756cfd9d7a44029762e42ac
Ansih Singh Walia <loveeanish@gmail.com>
2017-06-12 01:48:28 +0530

    Ridge Regression implemented

--- Ridge Regression and Lasso-Regularization Techniques.R
+++ Ridge Regression and Lasso-Regularization Techniques.R
@@ -9,3 +9,30 @@ attach(Hitters)
 
 x = model.matrix(Salary~.-1 , data = Hitters)#Predictors  
 y = Hitters$Salary #Response Variable to be used in Linear Model
+
+#First we will do Ridge Regression by setting alpha=0
+#In Ridge Regression difference is that it includes all the variables p
+#in the Models and does not includes a subset of variables.
+#So in shrinkage methods we will simply shrink the coefficient value towards
+#0 
+
+
+ridge<-glmnet(x, y ,alpha = 0)#Ridge Regression Model
+summary(ridge)
+ridge
+#Plotting lambda(Tuning Parameter) vs Cofficient values of variables.
+plot(ridge,xvar = "lambda",label = TRUE)
+#In Shrinkage techniques we will Shrink the Cofficient values towards 0 as
+#value of lambda increases to reduce the Error value
+
+#Corss validation with 10 folds
+cv.ridge = cv.glmnet(x,y,alpha=0)
+plot(cv.ridge)#all 20 variables in the Model-19 predictors + 1 intercept for each
+#Plot of MSE on Validation Set vs Lambda(Tuning Parameter)
+#Test Error first Decreases as RIDGE REGERSSION decreases the Model's variance
+#and complexity but after a point as lambda increases the Bias increases a
+#lot which causes Underfitting and again Error goes up.
+#With increase in Lambda the variance of Model decreases at cost of High Bias
+
+
+
