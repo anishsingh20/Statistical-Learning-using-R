065fcdf01c75816023ed0e4ec4ebcfc4f6ee69cd
Ansih Singh Walia <loveeanish@gmail.com>
2017-07-14 18:34:26 +0530

    Loaded the required Packages

--- DecisionTrees.R
+++ DecisionTrees.R
@@ -1,1 +1,11 @@
-#Desicion Trees in R
+#Desicion Trees in R ====================
+#Requiring Packages
+
+require(ISLR) #package containing data
+require(ggplot2)
+require(tree)
+
+#Using the Carseats data set 
+
+attach(Carseats)
+?Carseats
