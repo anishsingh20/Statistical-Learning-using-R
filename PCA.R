ee124613d9ba5607d9e2c77600db1e11551b0980
Ansih Singh Walia <loveeanish@gmail.com>
2017-08-31 11:58:55 +0530

    started with descriptive analysis

--- PCA.R
+++ PCA.R
@@ -1,1 +1,12 @@
 #unsupervised learning -PCA on UDArrests data set
+
+#we will use USAarrests data
+
+?USArrests
+#dataset which contains Violent Crime Rates by US State
+dim(USArrests)
+dimnames(USArrests)
+
+#finding mean of all 
+apply(USArrests,2,mean)
+apply(USArrests,2,var)
